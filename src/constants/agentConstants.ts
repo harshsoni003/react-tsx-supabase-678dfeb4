af33004855781f880414108c1856e736d99b3f40
harshcustomaistudio <harsh@customaistudio.io>
2025-06-19 16:41:30 +0530

    hdufh

--- src/constants/agentConstants.ts
+++ src/constants/agentConstants.ts
@@ -1,2 +1,2 @@
 // Agent constants used across the application
-export const DEFAULT_AGENT_ID = 'agent_01jy3gtnwhe4tb93f3qgb04ywc'; 
+export const DEFAULT_AGENT_ID = 'agent_01jy3xej5recpvs4a4vrxxd7yt'; 
